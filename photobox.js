57a61a5ddbf1dd2ff66e12f1db0e99c5e23003ac
ThomasDAP <95960414+ThomasDAP@users.noreply.github.com>
2022-03-07 15:29:20 +0100

    LE CHEVALIER NOIR VA LES SANCTIONNER

--- photobox.js
+++ photobox.js
@@ -1,6 +1,14 @@
 import photoloader from './lib/photoloader.js'
+import gallery from './lib/gallery.js'
+import gallerie_ui from './lib/gallery_ui.js'
 
 document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
-    photoloader.loadRessources();
+    gallery.load().then((data) => {
+      gallerie_ui.display_galerie(data);
+    })
+    
+    gallerie_ui.display_galerie(gallery.gallerie);
+    
   })
+
