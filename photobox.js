1aea7d048fc918da1bb48194c7b134ec7d08514a
ThomasDAP <95960414+ThomasDAP@users.noreply.github.com>
2022-03-21 14:29:12 +0100

    Heuss

--- photobox.js
+++ photobox.js
@@ -28,6 +28,41 @@ document.querySelector('#next').addEventListener('click', (e) => {
 })
 
 
+document.querySelector('#previous').addEventListener('click', (e) => {
+  console.log("Page precedente zbi");
+  console.log("On vide le dom");
+  document.querySelector("#gallery_container").innerHTML = "";
+  console.log("On affiche les nouvelles images");
+
+  gallery.prev().then(gallerie_ui.display_galerie)
+
+
+})
+
+
+document.querySelector('#first').addEventListener('click', (e) => {
+  console.log("Premiere page");
+  console.log("On vide le dom");
+  document.querySelector("#gallery_container").innerHTML = "";
+  console.log("On affiche les nouvelles images");
+
+  gallery.first().then(gallerie_ui.display_galerie)
+
+
+})
+
+document.querySelector('#last').addEventListener('click', (e) => {
+  console.log("Derniere page");
+  console.log("On vide le dom");
+  document.querySelector("#gallery_container").innerHTML = "";
+  console.log("On affiche les nouvelles images");
+
+  gallery.last().then(gallerie_ui.display_galerie)
+
+
+})
+
+
 
 
 
